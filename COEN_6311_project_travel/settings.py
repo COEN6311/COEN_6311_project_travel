b5a0083dc31b5bfb8931671688b9fd5b77fb2cad
bochaoli <bochaoli95@gmail.com>
2024-02-29 10:28:27 -0500

    resolve git conflict

--- COEN_6311_project_travel/settings.py
+++ COEN_6311_project_travel/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-vm-=#_91ku4p=7rpqkfx6ngs400g)yi68c2(5ry6m1!lha^)67
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.59.139.10','127.0.0.1']
+ALLOWED_HOSTS = ['13.59.139.10', '127.0.0.1', 'api.ipify.org']
 
 # Application definition
 
@@ -132,7 +132,6 @@ USE_I18N = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
