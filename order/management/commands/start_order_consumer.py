f304d4c888079f103feb23ebb90e4d178aaa7045
bochaoli <bochaoli95@gmail.com>
2024-02-28 23:28:20 -0500

    resolve git conflict

--- order/management/commands/start_order_consumer.py
+++ order/management/commands/start_order_consumer.py
@@ -1,12 +1,15 @@
 from django.core.management import BaseCommand
 import threading
 from order.mq.mq_consumer import start_consumer
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Starts the order consumer'
 
     def handle(self, *args, **options):
+        logger.info('Starting order consumer')
         print("Starts the order consumer!!!")
 
         # 定义一个新的线程
