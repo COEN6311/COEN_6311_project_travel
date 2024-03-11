78ce229c60f7f0e0b3331eb49210d03ed95bb031
bochaoli <bochaoli95@gmail.com>
2024-03-11 12:34:35 -0400

    fix price

--- order/serializers.py
+++ order/serializers.py
@@ -7,6 +7,7 @@ from rest_framework import serializers
 from order.constant import OrderStatus
 from order.models import UserOrder, AgentOrder
 from user.models import User
+from utils.number_util import calculate_price_taxed
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -18,25 +19,34 @@ class UserSerializer(serializers.ModelSerializer):
 class UserOrderSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     status = serializers.SerializerMethodField(read_only=True)
+    price = serializers.FloatField()  # or serializers.FloatField()
+    amount = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = UserOrder
-        fields = ['id', 'order_number', 'status', 'price', 'name', 'description', 'created_date', 'user']
+        fields = ['id', 'order_number', 'status', 'price', 'amount', 'name', 'description', 'created_date', 'user']
 
     def get_status(self, obj):
         status_code = obj.status
         return OrderStatus.get_description(status_code)
 
+    def get_amount(self, obj):
+        return float(calculate_price_taxed(obj.price))
 
 class AgentOrderSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     status = serializers.SerializerMethodField(read_only=True)
+    price = serializers.FloatField()  # or serializers.FloatField()
+    amount = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = AgentOrder
-        fields = ['id', 'order_number', 'agent_order_number', 'status', 'price', 'name', 'description', 'created_date',
-                  'user']
+        fields = ['id', 'order_number', 'agent_order_number', 'status', 'price', 'amount', 'name', 'description',
+                  'created_date', 'user']
 
     def get_status(self, obj):
         status_code = obj.status
         return OrderStatus.get_description(status_code)
+
+    def get_amount(self, obj):
+        return float(calculate_price_taxed(obj.price))
